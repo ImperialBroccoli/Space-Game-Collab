84fa093dddf2b3af81ef779fe9e2ec9f47418a32
ImperialBroccoli <jonathan.todd.sherman@gmail.com>
2024-08-26 13:22:55 -0500

    Project Setup

--- Assets/Code/Jon/Player/Ship_Movement_J.cs
+++ Assets/Code/Jon/Player/Ship_Movement_J.cs
@@ -7,11 +7,11 @@ public class Ship_Movement_J : MonoBehaviour
 
     void Start()
     {
-        Debug.Log("Cool Game"); // This is a really long comment and im really sad if your really going to read this when you really dont have the time arent you really supposed to be doing school right now well I guess maybe i should really be doing school right now but im distracted by writing this really long comment and you are also really really really distracted because you are reading this comment and why did you just waist a minute of your life reading this really really really long comment or do you have a life? these are the questions that have really plagued men for centuries like what is the purpose of life or what im thinking and your thinking right now "what is the real purpose of this really really long comment?" I really really hope you didnt read this whole comment. I guess since you made it to the end heres a rickroll: Never gonna give you up. Never gonna let you down...
+        Debug.Log(Tell_A_Joke()); // This is a really long comment and im really sad if your really going to read this when you really dont have the time arent you really supposed to be doing school right now well I guess maybe i should really be doing school right now but im distracted by writing this really long comment and you are also really really really distracted because you are reading this comment and why did you just waist a minute of your life reading this really really really long comment or do you have a life? these are the questions that have really plagued men for centuries like what is the purpose of life or what im thinking and your thinking right now "what is the real purpose of this really really long comment?" I really really hope you didnt read this whole comment. I guess since you made it to the end heres a rickroll: Never gonna give you up. Never gonna let you down...
     }
 
-    void Update()
+    string Tell_A_Joke()
     {
-        
+        return "Why dont programmers like nature? Because it has way to many bugs. ;)";
     }
 }
