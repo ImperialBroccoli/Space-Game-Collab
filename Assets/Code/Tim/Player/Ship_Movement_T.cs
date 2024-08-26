1864811014667ed654cc29385b73942f9834d405
ImperialBroccoli <jonathan.todd.sherman@gmail.com>
2024-08-26 10:46:02 -0500

    Project Setup

---
+++ Assets/Code/Tim/Player/Ship_Movement_T.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship_Movement_T : MonoBehaviour
+{
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
